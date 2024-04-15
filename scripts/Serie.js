1a22ba507de65769b081bb842b9adf9fa3f84b0a
marialeang2 <ma.angulom1@uniandes.edu.co>
2024-04-14 22:45:12 -0500

    primeraEntrega - :)

---
+++ scripts/Serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(refNumber, name, channel, seasons, description, link, image) {
+        this.refNumber = refNumber;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.link = link;
+        this.image = image;
+    }
+    return Serie;
+}());
+export { Serie };
---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+import { series } from "./data.js";
+var seriesTBody = document.getElementById('series');
+var averageTBody = document.getElementById('average');
+renderSeriesInTable(series);
+renderAverage(series);
+function renderSeriesInTable(series) {
+    series.forEach(function (c) {
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td>".concat(c.refNumber, "</td>\n                                <td>").concat(c.name, "</td>\n                                <td>").concat(c.channel, "</td>\n                                <td>").concat(c.seasons, "</td>");
+        seriesTBody.appendChild(trElement);
+    });
+}
+function renderAverage(series) {
+    var averageSeason = getAverageSeason(series);
+    var trElement = document.createElement("tr");
+    trElement.innerHTML = "<td colspan=\"3\">Season Average:</td><td>".concat(averageSeason, "</td>");
+    averageTBody.appendChild(trElement);
+}
+function getAverageSeason(series) {
+    var averageSeason = 0;
+    var sumSeasons = 0;
+    series.forEach(function (serie) { return sumSeasons = sumSeasons + serie.seasons; });
+    averageSeason = sumSeasons / series.length;
+    return averageSeason;
+}
